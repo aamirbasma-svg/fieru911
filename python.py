2eeb8a7e0152c8d0a571e49c980db5f37bdeb83d
aamirbasma-svg <aamirbasma@gmail.com>
2026-06-05 19:01:49 +0500

    Box Plot

--- python.py
+++ python.py
@@ -4,9 +4,9 @@ x = [3, 5, 2, 8, 7, 9, 3, 4, 6, 1]
 y= []
 for i in x:
     y.append(3*i+2)
-# Line Chart
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Line Chart')
+# Box Plot
+plt.boxplot(x)
+plt.ylabel('Values')
+plt.title('Box Plot')
 plt.show()
+
